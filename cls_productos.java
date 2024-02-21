a3d45ceddbdab0b0879f8abc99faff1e132e9666
Jose <jose.ochoaov@amigo.edu.co>
2024-02-21 16:57:29 -0500

    creando commit

---
+++ cls_productos.java
@@ -0,0 +1,7 @@
+public class cls_productos {
+    private int codigoint;
+    private String nombreStr;
+    private int existenciasint;
+    private float precio_ventaflt;
+    private float gananciasflt;
+}
